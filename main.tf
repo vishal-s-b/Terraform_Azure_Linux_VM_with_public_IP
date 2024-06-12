98a3ba2ba3d54abfea36c98bdf55b36ac85c9f8c
Vishal Singh Bhardvaj <123397908+vishal-s-b@users.noreply.github.com>
2024-06-12 09:24:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "2222"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
